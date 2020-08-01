6e41e004e18194856ee2cabc6485942d1314acfe
anubhav <chauhananubhav16@gmail.com>
2020-08-02 02:36:11 +0530

    changes

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { connect } from "react-redux";
 
-
 const Home = (state) => {
-    return (
-        <div>
-            HOME
-        </div>
-    )
-}
+  return <div>HOME</div>;
+};
 
-const mapStateToProps = state => ({ state })
+const mapStateToProps = (state) => ({ state });
 
 export default connect(mapStateToProps, null)(Home);
